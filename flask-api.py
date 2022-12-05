56a106eb5e75513091fd4a44370332268ce7532d
JesusUrias <jesusuriasp@gmail.com>
2022-12-05 12:26:33 -0700

    cambio de dataset y creacion de api

---
+++ flask-api.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from flask_jsonpify import jsonpify
+import json
+import main
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Hello World!"
+
+@app.route('/game', methods=["GET"])
+def recommend_game():
+    game = request.args.get('Game')
+    game_recommendation = main.gameRec(game)
+    # convertimos la salida a JSON    
+    return jsonify({'data': json.dumps(game_recommendation['similarity'].to_dict())})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+   
--- main.py
+++ main.py
@@ -4,13 +4,41 @@ import sklearn as sk
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 
-game_reviews_df = pd.read_csv('steam_reviews.csv')
-# print(game_reviews_df)
+game_reviews_df = pd.read_csv('steam-200k.csv')
+#print(game_reviews_df)
 
-clean_game_reviews_df = game_reviews_df.drop(['review', 'is_early_access_review'], axis=1)
+clean_game_reviews_df = game_reviews_df.drop(['0', 'purchase'], axis=1)
+clean_game_reviews_df = clean_game_reviews_df.rename({'151603712': 'User', 'The Elder Scrolls V Skyrim': 'Game', '1.0': 'PlayTime'}, axis=1)
 
+# clean_game_reviews_df.replace({"Not Recommended": 0, "Recommended": 1}, inplace=True)
+#print(clean_game_reviews_df)
+matrix = clean_game_reviews_df.pivot_table(columns='Game', index='User', values='PlayTime', fill_value=0)
+matrix = matrix.dropna(thresh=5, axis=0)
 
-clean_game_reviews_df.replace({"Not Recommended": 0, "Recommended": 1}, inplace=True)
-# print(clean_game_reviews_df)
-matrix = clean_game_reviews_df.pivot_table(columns='title', index='date_posted', values='recommendation')
 print(matrix)
+
+def center(row):
+    new_row = (row - row.mean()) / (row.max() - row.min())
+    return new_row
+
+matrix_std = matrix.apply(center)
+# print(matrix_std)
+
+# matrix.loc[clean_game_reviews_df['title']] == 'ARK: Survival Evolved'
+
+def gameRec(game):
+    centered_matrix_values = matrix_std[game]
+    #Calcula la correlación de pearson de un juego con los demás
+    centered_matrix_values = matrix.corrwith(centered_matrix_values).dropna()
+    #crea un dataframe que muestra las veces que cada juego ha sido jugado y la media del tiempo que ha sido jugado
+    gameData = clean_game_reviews_df.groupby('Game').agg({'PlayTime': [np.size, np.mean]})
+    #print('game data')
+    #print(gameData)
+    #filtro que elimina los juegos jugados por menos de 100 usuarios.
+    gameSim = gameData['PlayTime']['size'] >= 100
+    df = gameData[gameSim].join(pd.DataFrame(centered_matrix_values, columns=['similarity']), ['Game'])
+    df_descending = df.sort_values(['similarity'], ascending=False)[:6]
+    #return df_descending.to_dict()
+    return df.sort_values(['similarity'], ascending=False)[:6]
+
+print(gameRec("Borderlands 2"))
