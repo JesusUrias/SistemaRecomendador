53b9c278bafa54a011076f548b0ea84114ad23ad
JesusUrias <jesusuriasp@gmail.com>
2022-12-05 20:45:59 -0700

    push para subir la api a heroku

--- flask-api.py
+++ flask-api.py
@@ -14,7 +14,7 @@ def recommend_game():
     game = request.args.get('Game')
     game_recommendation = main.gameRec(game)
     # convertimos la salida a JSON    
-    return jsonify({'data': json.dumps(game_recommendation['similarity'].to_dict())})
+    return jsonify({'game': json.dumps(game_recommendation['similarity'].to_dict())})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
